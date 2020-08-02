3b2b5a8f3fd38276e389487a49591183d8026682
vunguyen <vqnguyen9@uh.edu>
2020-08-02 13:04:03 -0400

    Almost Done - Click button to show prediction score
    Click button to show prediction score

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,84 @@
-import { Component} from '@angular/core';
-import * as $ from 'jquery';
+import { Component, OnInit} from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'Twitter Sentiment App';
   sentimenttext = 'Put something here';
-  prediction = 0;
+  predictions;
+  prediction;
   changeLog: any;
+  model;
+  dict;
+  word_index;
+
+  public async ngOnInit() {
+    this.dict = await $.ajax({url: '/assets/dict.csv',dataType: 'text',});
+    await this.success(this.dict);
+    this.model = await tf.loadLayersModel('/assets/model.json');
+    console.log(this.model.summary());
+  }
+
+  success(data){
+    var wd_idx = new Object();
+    var lst = data.split(/\r?\n|\r/);
+    for(var i = 0 ; i < lst.length ;i++){
+        var key = (lst[i]).split(',')[0];
+        var value = (lst[i]).split(',')[1];
+        if(key == "")
+            continue
+        wd_idx[key] = parseInt(value);
+    }
+    
+    this.word_index = wd_idx
+    console.log(this.word_index);
+  }
+
+  process(txt){
+      var out = txt.replace(/[^a-zA-Z0-9\s]/, '');
+      out = out.trim().split(/\s+/);
+      for (var i = 0 ; i < out.length ; i++)
+        out[i] = out[i].toLowerCase();
+      return out
+  }
+
+  create_sequences(txt){
+      var max_tokens = 40;
+      var tokens = [];
+      var words = this.process(txt);
+      var seq = Array.from(Array(max_tokens), () => 0) 
+      var start = max_tokens-words.length
+      for(var i= 0 ; i< words.length ; i++){
+          if (Object.keys(this.word_index).includes(words[i])){
+              seq[i+start] = this.word_index[words[i]]
+          }    
+          
+      }
+      return seq
+  }
+
 
   textchanged(value) {
     this.sentimenttext = value;
-    this.predict();
   }
 
   predict() {
-    if (this.sentimenttext.length < 10) {
-      this.prediction = this.sentimenttext.length;
-    } else {
-      this.prediction = -this.sentimenttext.length;
-    }
+    console.log('Button Clicked')
+    var seq = this.create_sequences(this.sentimenttext);
+    console.log('Seq Created');
+    var input = tf.tensor(seq);
+    input = input.expandDims(0);
+    console.log('Input Created');
+    this.predictions = this.model.predict(input);
+    this.prediction = this.predictions.toFloat();
+    console.log('Model output');
+    console.log('predictions ', this.predictions);
+    console.log('prediction ', this.prediction);
   }
 }
