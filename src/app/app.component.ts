9628ba9372db74a506b1d92c945d6c4510d9bb89
vunguyen <vqnguyen9@uh.edu>
2020-08-02 13:57:25 -0400

    Fix async
    Button is now clickable on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit} from '@angular/core';
 import * as tf from '@tensorflow/tfjs';
 import * as $ from 'jquery'
 
+// Guide: 
+// https://towardsdatascience.com/how-to-use-angular-to-deploy-tensorflow-web-apps-5675b5a042cc
+// https://medium.com/@alyafey22/sentiment-classification-from-keras-to-the-browser-7eda0d87cdc6
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,33 +15,18 @@ import * as $ from 'jquery'
 export class AppComponent implements OnInit {
   title = 'Twitter Sentiment App';
   sentimenttext = 'Put something here';
-  predictions;
-  prediction;
   changeLog: any;
   model;
   dict;
   word_index;
+  predictions;
+  prediction;
 
   public async ngOnInit() {
     this.dict = await $.ajax({url: '/assets/dict.csv',dataType: 'text',});
-    await this.success(this.dict);
     this.model = await tf.loadLayersModel('/assets/model.json');
-    console.log(this.model.summary());
-  }
-
-  success(data){
-    var wd_idx = new Object();
-    var lst = data.split(/\r?\n|\r/);
-    for(var i = 0 ; i < lst.length ;i++){
-        var key = (lst[i]).split(',')[0];
-        var value = (lst[i]).split(',')[1];
-        if(key == "")
-            continue
-        wd_idx[key] = parseInt(value);
-    }
-    
-    this.word_index = wd_idx
-    console.log(this.word_index);
+    // console.log(this.model.summary());
+    this.word_index = await success(this.dict);
   }
 
   process(txt){
@@ -63,22 +52,38 @@ export class AppComponent implements OnInit {
       return seq
   }
 
-
   textchanged(value) {
     this.sentimenttext = value;
+    // this.predict();
   }
 
   predict() {
+    this.prediction = 0;
     console.log('Button Clicked')
     var seq = this.create_sequences(this.sentimenttext);
     console.log('Seq Created');
     var input = tf.tensor(seq);
     input = input.expandDims(0);
     console.log('Input Created');
-    this.predictions = this.model.predict(input);
-    this.prediction = this.predictions.toFloat();
+    const output = this.model.predict(input);
+    this.predictions = Array.from(output.dataSync());
+    this.prediction = this.predictions[0];
+    // this.prediction = this.predictions.toFloat();
     console.log('Model output');
     console.log('predictions ', this.predictions);
     console.log('prediction ', this.prediction);
   }
+};
+
+async function success(data){
+  var wd_idx = new Object();
+  var lst = data.split(/\r?\n|\r/);
+  for(var i = 0 ; i < lst.length ;i++){
+      var key = (lst[i]).split(',')[0];
+      var value = (lst[i]).split(',')[1];
+      if(key == "")
+          continue
+      wd_idx[key] = parseInt(value);
+  }
+  return wd_idx
 }
